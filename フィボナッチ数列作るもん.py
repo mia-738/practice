614179114df5a13be8d0e6f905df694abb91cdfd
mia33501 <85103168+mia33501@users.noreply.github.com>
2022-03-18 00:07:13 +0900

    Update フィボナッチ数列作るもん.py

--- フィボナッチ数列作るもん.py
+++ フィボナッチ数列作るもん.py
@@ -1,9 +1,19 @@
-a=""
+L a=""
 b=""
 c=0
+d=0
 
 while True:
     a=input()
     b=input()
-    if a<0 or b<0:
-        
+    if int(a)<0 or int(b)<0:
+        print(整数を入力してください)
+    else:
+        break
+
+while int(d)<50:
+    c=int(a)+int(b)
+    print(c)
+    a=b
+    b=c
+    d++
