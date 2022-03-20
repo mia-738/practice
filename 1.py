82f9c0c2ba7867865663cd60cb0cade99fb1cded
mia33501 <85103168+mia33501@users.noreply.github.com>
2022-03-20 16:46:49 +0900

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,12 +6,14 @@ import os
 a=""
 bina=0
 b=""
+b1=""
 c=65535
 c1=65535
 d=0
 d1=0
 e=0
 f=0
+f1=0
 
 while True:
     a=input()
@@ -27,10 +29,19 @@ while True:
         else:
             break
 
-b=math.floor(random.random()*1000000001)
-c<<=len(bin(b))-18
+b1=10**random.randrange(0,20)+1
+b=math.floor(random.random()*b1)
+
+if len(bin(int(b)))<len(bin(int(c))):
+    c>>=18-len(bin(b))
+else:
+    c<<=len(bin(b))-18
+    f=1
+
 d=b&c
 
+if f==1:
+    d>>=len(bin(b))-18
 
 bina=bin(int(a))
 
@@ -38,13 +49,11 @@ if len(bin(int(a)))<len(bin(int(c1))):
     c1>>=18-len(bina)
 else:
     c1<<=len(bina)-18
-    f=1
+    f1=1
 
 d1=int(a)&c1
 
-d>>=len(bin(b))-18
-
-if f==1:
+if f1==1:
     d1>>=len(bina)-18
 
 e=d^d1
