3d5790928cfade8de99f9d1648ca7ea32c3706c3
mia33501 <85103168+mia33501@users.noreply.github.com>
2023-01-21 22:58:18 +0900

    Create sample.py

---
+++ 素数なんちゃら/sample.py
@@ -0,0 +1,10 @@
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    return [x for x in range(n + 1) if primes[x]]
+
+print(sieve_of_eratosthenes(1000000))
