96aede9f32b64d3b3a7792dd132a2eb605c0d271
mia33501 <85103168+mia33501@users.noreply.github.com>
2023-01-22 14:11:42 +0900

    Update 素数判定(普通).py

--- 素数なんちゃら/素数判定(普通).py
+++ 素数なんちゃら/素数判定(普通).py
@@ -1,31 +1,15 @@
-import re
-import os
 b=1
 
-while True:
-    a=input()
-
-    if re.compile('[a-z]+').fullmatch(a):
-
-        print("\n数を入力してください\n")
+def prime(a):
+    if int(a)<2:
+        return False
+    elif int(a)==2:
+        return True
 
     else:
-        break
-
-
-if int(a)<2:
-    print("素数ではない")
-elif int(a)==2:
-    print("素数")
-
-else:
-    while True:
-        b+=1
-        if int(a)%b==0:
-            print("素数ではない")
-            break
-        elif b==int(a)-1:
-            print("素数")
-            break
-
-os.system('PAUSE')
+        while True:
+            b+=1
+            if int(a)%b==0:
+                return False
+            elif b==int(a)-1:
+                return True
