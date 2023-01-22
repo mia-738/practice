fd9227bfae7657512cb08a257b9e78d63226777e
mia33501 <85103168+mia33501@users.noreply.github.com>
2023-01-22 14:01:05 +0900

    Update フィボナッチ数列作るもん.py

--- フィボナッチ数列作るもん.py
+++ フィボナッチ数列作るもん.py
@@ -1,24 +1,18 @@
-import os
-
-a=""
-b=""
 c=0
 d=0
 
-while True:
-    a=input()
-    b=input()
-    if int(a)<0 or int(b)<0:
-        print("\n\"整数を\"入力してください\n")
-    else:
-        break
+def Fibonacci(a,b):
+    while True:
+        if int(a)<0 or int(b)<0:
+            print("\n\"整数を\"入力してください\n")
+        else:
+            break
 
-while int(d)<50:
-    c=int(a)+int(b)
-    print(c)
-    a=b
-    b=c
-    d+=1
+    while int(d)<50:
+        c=int(a)+int(b)
+        print(c)
+        a=b
+        b=c
+        d+=1
 
-print("")
-os.system('PAUSE')
+    print("")
