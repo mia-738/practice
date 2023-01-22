7ae72f60aafe104c5cbd5fa7658c1c7b32bbe80e
mia33501 <85103168+mia33501@users.noreply.github.com>
2023-01-22 14:08:16 +0900

    Update 素数判定(改良).py

--- 素数なんちゃら/素数判定(改良).py
+++ 素数なんちゃら/素数判定(改良).py
@@ -1,32 +1,18 @@
-import re
 import math
-import os
 f=""
 x=1
-while True:
-    a=input()
-    if re.compile('[a-z]+').fullmatch(a):
-        print("\n数を入力してください\n")
+def is_prime(a):
+    if int(a)<=1:
+        return False
+    elif a==2 or a==3:
+        return True
+    elif int(a)%2==0:
+        return False
     else:
-        break
-if int(a)<=1:
-    f=0
-elif a==2 or a==3:
-    f=1
-elif int(a)%2==0:
-    f=0
-else:
-    b=math.floor(math.sqrt(int(a)))
-    while True:
-        x+=2
-        if int(a)%x==0:
-            f=0
-            break
-        elif x>=b:
-            f=1
-            break
-if f==0:
-    print("素数ではない")
-elif f==1:
-    print("素数")
-os.system('PAUSE')
+        b=math.floor(math.sqrt(int(a)))
+        while True:
+            x+=2
+            if int(a)%x==0:
+                return False
+            elif x>=b:
+                return True
