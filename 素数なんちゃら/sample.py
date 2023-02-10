2f66b2339ab975d7d364bd406887797b7f95ae65
mia33501 <85103168+mia33501@users.noreply.github.com>
2023-02-10 20:29:40 +0900

    Update sample.py

--- 素数なんちゃら/sample.py
+++ 素数なんちゃら/sample.py
@@ -1,10 +1,9 @@
 #エラトステネスの篩
 
 def sieve_of_eratosthenes(n):
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
+    if n <= 1:
+        return False
     for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
-    return [x for x in range(n + 1) if primes[x]]
+        if n % i == 0:
+            return False
+    return True
